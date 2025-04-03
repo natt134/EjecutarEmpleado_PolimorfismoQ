09e644a22bf6d66fba86bddde39a2a853e2c9bbd
natal <natal@192.168.1.101>
2025-04-02 21:23:29 -0500

    EjecutarEmpleado_PolimorfismoQ

---
+++ src/ejecutarempleado/Programadores.java
@@ -0,0 +1,21 @@
+
+package ejecutarempleado;
+
+/**
+ *
+ * @author natal
+ */
+class Programadores extends Empleados{
+
+    
+    
+   
+public void TotalAPagar (){
+Total=Salario;
+}
+
+    public double getTotal() {
+        return Total;
+    }
+
+}
